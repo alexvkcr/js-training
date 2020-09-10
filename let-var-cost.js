c65cc655c1f026175b570517953470d7d46d1d72
Alejandro Díaz <alexvkcr@gmail.com>
2020-09-10 13:44:45 +0200

    feat/let and var added

--- let-var-cost.js
+++ let-var-cost.js
@@ -7,3 +7,17 @@ const dog = {
 
 dog.name = "Fernando Alonso";
 //YES, iti is changed
+
+function test() {
+  var num_var = 2;
+  let num_let = 2;
+
+  console.log("in the same block");
+  console.log("var =" + num_var);
+  console.log("let =" + num_let);
+}
+
+test();
+console.log("out of the block");
+console.log("var =" + num_var);
+console.log("let =" + num_let);
